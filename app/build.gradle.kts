97ecc34624298e45e1fbda6cefad9192d9e4200c
WangZhiYao <393893729@qq.com>
2024-05-20 21:46:49 +0800

    update(version): v2.0
    Signed-off-by: WangZhiYao <393893729@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -18,8 +15,8 @@ android {
         applicationId = "com.paperloong.illuminometer"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
