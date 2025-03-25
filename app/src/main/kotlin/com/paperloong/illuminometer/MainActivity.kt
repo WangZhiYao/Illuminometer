ead41ee489f8ac992bb0439ec11c50337e3a111f
WangZhiYao <393893729@qq.com>
2025-03-25 10:24:00 +0800

    update(theme): add enableEdgeToEdge
    Signed-off-by: WangZhiYao <393893729@qq.com>

--- app/src/main/kotlin/com/paperloong/illuminometer/MainActivity.kt
+++ app/src/main/kotlin/com/paperloong/illuminometer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.paperloong.illuminometer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.paperloong.illuminometer.ui.IlluminometerApp
@@ -19,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             IlluminanceTheme {
                 IlluminometerApp()
