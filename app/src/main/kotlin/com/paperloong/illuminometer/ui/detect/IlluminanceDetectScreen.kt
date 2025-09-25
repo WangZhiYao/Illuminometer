a153e029a84e456d6944b546a2fa2e947b039e45
WangZhiYao <393893729@qq.com>
2025-09-25 23:56:17 +0800

    refactor: Use `hiltViewModel` from `androidx.hilt.lifecycle.viewmodel.compose`
    The import for `hiltViewModel` has been updated from `androidx.hilt.navigation.compose.hiltViewModel` to `androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel` in `DetectRecordScreen.kt` and `IlluminanceDetectScreen.kt`.

--- app/src/main/kotlin/com/paperloong/illuminometer/ui/detect/IlluminanceDetectScreen.kt
+++ app/src/main/kotlin/com/paperloong/illuminometer/ui/detect/IlluminanceDetectScreen.kt
@@ -45,7 +45,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
